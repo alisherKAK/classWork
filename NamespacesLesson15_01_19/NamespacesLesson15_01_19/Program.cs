b2138a609db4f511702d3171a076c6a745c6b232
Калабаев Алишер Куанышкалиулы <@corp.itstep>
2019-01-15 21:40:04 +0600

    15.01.19 Class Work

---
+++ NamespacesLesson15_01_19/NamespacesLesson15_01_19/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SomeLib;
+
+namespace NamespacesLesson15_01_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            //try
+            //{
+            //    ManagerService.Operate();
+            //}
+            //catch(ArgumentException exception)
+            //{
+            //    Console.WriteLine(exception.Message);
+            //}
+
+            //Console.WriteLine("Код работает дальше, не смотря на ошибку");
+
+            /*try
+            {
+                string ageAsString = Console.ReadLine();
+                int age = int.Parse(ageAsString);
+
+                throw new NotImplementedException();
+
+                //код ниже в этом блоке не будет работать
+            }
+            catch (FormatException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (ArgumentException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            finally
+            {
+
+            }*/
+
+        }
+    }
+}
---
+++ NamespacesLesson15_01_19/NamespacesLesson15_01_19/Sms.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Find your Account Sid and Token at twilio.com/console
+        const string accountSid = "ACaf5825a2de837d652356021ed33d5f68";
+        const string authToken = "your_auth_token";
+
+        TwilioClient.Init(accountSid, authToken);
+
+        var message = MessageResource.Create(
+            body: "This is the ship that made the Kessel Run in fourteen parsecs?",
+            from: new Twilio.Types.PhoneNumber("+15017122661"),
+            to: new Twilio.Types.PhoneNumber("+15558675310")
+        );
+
+        Console.WriteLine(message.Sid);
+    }
+}
