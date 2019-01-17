cc4e004ad92496381ae52cb4c31b97cb53520dad
Калабаев Алишер Куанышкалиулы <@corp.itstep>
2019-01-17 21:41:44 +0600

    Home Work 27.01.19

---
+++ HomeWork27_01_19/HomeWork27_01_19/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork27_01_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = new int[1];
+            mas[0] = 12;
+            Console.WriteLine(mas[1]);
+        }
+    }
+}
---
+++ HomeWork27_01_19/HomeWork27_01_19/RangeOfArray.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork27_01_19
+{
+    public class RangeOfArray
+    {
+        #region Свойства
+        public int RangeStart { get; set; }
+        public int RangeEnd { get; set; }
+        public int[] Array { get; set; }
+        public int this [int index]
+        {
+            get
+            {
+                if (index >= RangeStart && index <= RangeEnd)
+                {
+                    return Array[index];
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException("Не входите в диапозон!");
+                }
+            }
+
+            set
+            {
+                if (index >= RangeStart && index <= RangeEnd)
+                {
+                    Array[index] = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException("Не входите в диапозон!");
+                }
+            }
+        }
+        #endregion
+
+        #region Конструкторы
+        public RangeOfArray() { RangeStart = 0; }
+        #endregion
+    }
+}
