37023b542b20fd00d8dcaca96ea1b2dbaaf81638
Калабаев Алишер Куанышкалиулы <@corp.itstep>
2019-03-07 21:33:15 +0600

    16.03.19 PracticeWork

---
+++ HomeWork16_03_19/HomeWork16_03_19/Card.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork16_03_19
+{
+    public class Card
+    {
+        public CardTypes Type { get; set; }
+        public Lears Lear { get; set; }
+    }
+}
