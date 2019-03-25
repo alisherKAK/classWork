3892680537d9c926cbb97af3293883e1ce1ca78e
Калабаев Алишер Куанышкалиулы <@corp.itstep>
2019-03-25 21:41:36 +0600

    StarWars

---
+++ StarWars/StarWars/Character.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarWars
+{
+    [Serializable]
+    public class Character
+    {
+        [JsonIgnore]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("height")]
+        public string Height { get; set; }
+
+        [JsonProperty("mass")]
+        public string Mass { get; set; }
+
+        [JsonProperty("hair_color")]
+        public string Hair_color { get; set; }
+
+        [JsonProperty("skin_color")]
+        public string Skin_color { get; set; }
+
+        [JsonProperty("eye_color")]
+        public string Eye_color { get; set; }
+
+        [JsonProperty("birth_year")]
+        public string Birth_year { get; set; }
+
+        [JsonProperty("gender")]
+        public string Gender { get; set; }
+
+        [JsonProperty("homeworld")]
+        public string Homeworld { get; set; }
+
+        [JsonProperty("films")]
+        public List<string> Films { get; set; }
+
+        [JsonProperty("species")]
+        public List<string> Species { get; set; }
+
+        [JsonProperty("vehicles")]
+        public List<string> Vehicles { get; set; }
+
+        [JsonProperty("starships")]
+        public List<string> Starships { get; set; }
+
+        [JsonProperty("created")]
+        public DateTime Created { get; set; }
+
+        [JsonProperty("edited")]
+        public DateTime Edited { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+    }
+}
---
+++ StarWars/StarWars/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Newtonsoft.Json;
+using System.Xml.Serialization;
+
+namespace StarWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = ChoseNumberOfPerson();
+            int index = 0;
+
+            string result = "";
+            string requestString = $@"people/{number.ToString()}/";
+            List<Character> characters = new List<Character>();
+
+            if (!IsCharacterHave(number, characters, ref index))
+            {
+                WebRequest request = WebRequest.Create("https://swapi.co/api/" + requestString);
+                WebResponse response = request.GetResponse();
+                using (Stream stream = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        result = reader.ReadToEnd();
+                    }
+                }
+                response.Close();
+
+                Character newCharacter = JsonConvert.DeserializeObject<Character>(result);
+
+                var serializer = new XmlSerializer(typeof(List<Character>));
+
+                using (var stream = File.Create("file.xml"))
+                {
+                    serializer.Serialize(stream, characters);
+                }
+
+                Console.WriteLine($"{newCharacter.Name}\n" +
+                                  $"{newCharacter.Height}\n" +
+                                  $"{newCharacter.Mass}");
+            }
+            else
+            {
+                Console.WriteLine($"{characters[index].Name}\n" +
+                                      $"{characters[index].Height}\n" +
+                                      $"{characters[index].Mass}");
+            }
+
+        }
+        static int ChoseNumberOfPerson()
+        {
+            try
+            {
+                Console.WriteLine("Введите номер перонажа которого вы хотите получить");
+
+                int number;
+
+                if(int.TryParse(Console.ReadLine().Trim(), out number))
+                {
+                    return number;
+                }
+
+                throw new ArgumentException("Число было введено неверно");
+            }
+            catch(ArgumentException exception)
+            {
+                Console.WriteLine(exception.Message);
+
+                return ChoseNumberOfPerson();
+            }
+        }
+        static bool IsCharacterHave(int number, List<Character> characters, ref int index)
+        {
+            var serializer = new XmlSerializer(typeof(List<Character>));
+            List<Character> deserializeResult;
+
+            using (var stream = File.OpenRead("file.xml"))
+            {
+                deserializeResult = serializer.Deserialize(stream) as List<Character>;
+            }
+
+            characters = deserializeResult;
+
+            for (int i = 0; i < deserializeResult.Count; i++)
+            {
+                if (deserializeResult[i].Id == number)
+                {
+                    index = i;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
