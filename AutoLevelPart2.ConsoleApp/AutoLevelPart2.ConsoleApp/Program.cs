51bc3627b9d1a006053f0a9b69cda4bf4554c0f3
Калабаев Алишер Куанышкалиулы <@corp.itstep>
2019-04-15 21:32:08 +0600

    Home Work

---
+++ AutoLevelPart2.ConsoleApp/AutoLevelPart2.ConsoleApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Common;
+using System.Configuration;
+
+namespace AutoLevelPart2.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = ConfigurationManager.ConnectionStrings["appConnection"];
+            var providerName = configuration.ProviderName;
+            var connectionString = configuration.ConnectionString;
+
+            var providerFactory = DbProviderFactories.GetFactory(providerName);
+            using(var connection = providerFactory.CreateConnection())
+            {
+                connection.ConnectionString = connectionString;
+
+                var dataSet = new DataSet("users");
+                var dataAdapter = providerFactory.CreateDataAdapter();
+
+                var selectUsersCommand = connection.CreateCommand();
+                selectUsersCommand.CommandText = "select * from Users";
+                dataAdapter.SelectCommand = selectUsersCommand;
+
+                dataAdapter.Fill(dataSet, "Users");
+
+                var commandBuilder = providerFactory.CreateCommandBuilder();
+                commandBuilder.DataAdapter = dataAdapter;
+
+                var usersTable = dataSet.Tables["Users"];
+                var row = usersTable.Rows[0];
+                row.BeginEdit();
+                row["Login"] = "superUser";
+                row.EndEdit();
+
+                dataAdapter.Update(dataSet, "Users");
+            }
+        }
+    }
+}
